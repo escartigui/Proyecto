37c9486886ccd60c3474fe9dd9e4a4594cd45b7c
escartigui <escarlethmayali@gmail.com>
2025-08-15 21:31:24 -0600

    scar

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,10 @@
-print("danis")
+class Productos:
+    def __init__(self, codigo,nombre,categoria,precio,stock):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
+        self.stock = stock
+
+    def mostrar_productos(self):
+        return f"Codigo{self.codigo},Nombre{self.nombre} Categoria{self.categoria} Precio{self.precio} Stock{self.stock}"
